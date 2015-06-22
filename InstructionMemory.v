ea77d9783ecde73049888fa6d5195057764484ca
Nuullll <vfirst218@gmail.com>
2015-06-22 17:40:34 +0800

    add new InstructionMemory.v

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -5,32 +5,45 @@ module InstructionMemory(Address, Instruction);
 	
 	always @(*)
 		case (Address[9:2])
-			// addi $a0, $zero, 12345 #(0x3039)
-			8'd0:    Instruction <= {6'h08, 5'd0 , 5'd4 , 16'h3039};
-			// addiu $a1, $zero, -11215 #(0xd431)
-			8'd1:    Instruction <= {6'h09, 5'd0 , 5'd5 , 16'hd431};
-			// sll $a2, $a1, 16
-			8'd2:    Instruction <= {6'h00, 5'd0 , 5'd5 , 5'd6 , 5'd16 , 6'h00};
-			// sra $a3, $a2, 16
-			8'd3:    Instruction <= {6'h00, 5'd0 , 5'd6 , 5'd7 , 5'd16 , 6'h03};
-			// beq $a3, $a1, L1
-			8'd4:    Instruction <= {6'h04, 5'd7 , 5'd5 , 16'h0001};
-			// lui $a0, -11111 #(0xd499)
-			8'd5:    Instruction <= {6'h0f, 5'd0 , 5'd4 , 16'hd499};
-			// L1:
-			// add $t0, $a2, $a0
-			8'd6:    Instruction <= {6'h00, 5'd6 , 5'd4 , 5'd8 , 5'd0 , 6'h20};
-			// sra $t1, $t0, 8
-			8'd7:    Instruction <= {6'h00, 5'd0 , 5'd8 , 5'd9 , 5'd8 , 6'h03};
-			// addi $t2, $zero, -12345 #(0xcfc7)
-			8'd8:    Instruction <= {6'h08, 5'd0 , 5'd10, 16'hcfc7};
-			// slt $v0, $a0, $t2
-			8'd9:    Instruction <= {6'h00, 5'd4 , 5'd10 , 5'd2 , 5'd0 , 6'h2a};
-			// sltu $v1, $a0, $t2
-			8'd10:   Instruction <= {6'h00, 5'd4 , 5'd10 , 5'd3 , 5'd0 , 6'h2b};
+			// addi $a0, $zero, 3
+			8'd0:    Instruction <= 32'h20040003;
+			// jal sum
+			8'd1:    Instruction <= 32'h0c100003;
 			// Loop:
-			// j Loop
-			8'd11:   Instruction <= {6'h02, 26'd11};
+			// beq $zero, $zero, Loop
+			8'd2:    Instruction <= 32'h1000ffff;
+			// sum:
+			// addi $sp, $sp, -8
+			8'd3:    Instruction <= 32'h23bdfff8;
+			// sw $ra, 4($sp)
+			8'd4:    Instruction <= 32'hafbf0004;
+			// sw $a0, 0($sp)
+			8'd5:    Instruction <= 32'hafa40000;
+			// slti $t0, $a0, 1
+			8'd6:    Instruction <= 32'h28880001;
+			// beq $t0, $zero, L1
+			8'd7:    Instruction <= 32'h11000003;
+			// xor $v0, $zero, $zero
+			8'd8:    Instruction <= 32'h00001026;
+			// addi $sp, $sp, 8
+			8'd9:    Instruction <= 32'h23bd0008;
+			// jr $ra
+			8'd10:   Instruction <= 32'h03e00008;
+			// L1:
+			// addi $a0, $a0, -1
+			8'd11:   Instruction <= 32'h2084ffff;
+			// jal sum
+			8'd12:   Instruction <= 32'h0c100003;
+			// lw $a0, 0($sp)
+			8'd13:   Instruction <= 32'h8fa40000;
+			// lw $ra, 4($sp)
+			8'd14:   Instruction <= 32'h8fbf0004;
+			// addi $sp, $sp, 8
+			8'd15:   Instruction <= 32'h23bd0008;
+			// add $v0, $a0, $v0
+			8'd16:   Instruction <= 32'h00821020;
+			// jr $ra
+			8'd17:   Instruction <= 32'h03e00008;
 			
 			default: Instruction <= 32'h00000000;
 		endcase
--- README.md
+++ README.md
@@ -438,7 +438,63 @@ L1:
     
     + 立即数-1和-8分别被翻译成`0xffff`和`0xfff8`。
     
-3. 
+3. 修改`InstructionMemory.v`如下
+
+    ```verilog
+        
+    module InstructionMemory(Address, Instruction);
+	    input [31:0] Address;
+	    output reg [31:0] Instruction;
+	
+	    always @(*)
+		    case (Address[9:2])
+			    // addi $a0, $zero, 3
+			    8'd0:    Instruction <= 32'h20040003;
+			    // jal sum
+			    8'd1:    Instruction <= 32'h0c100003;
+			    // Loop:
+			    // beq $zero, $zero, Loop
+			    8'd2:    Instruction <= 32'h1000ffff;
+			    // sum:
+			    // addi $sp, $sp, -8
+			    8'd3:    Instruction <= 32'h23bdfff8;
+			    // sw $ra, 4($sp)
+			    8'd4:    Instruction <= 32'hafbf0004;
+			    // sw $a0, 0($sp)
+			    8'd5:    Instruction <= 32'hafa40000;
+			    // slti $t0, $a0, 1
+			    8'd6:    Instruction <= 32'h28880001;
+			    // beq $t0, $zero, L1
+			    8'd7:    Instruction <= 32'h11000003;
+			    // xor $v0, $zero, $zero
+			    8'd8:    Instruction <= 32'h00001026;
+			    // addi $sp, $sp, 8
+			    8'd9:    Instruction <= 32'h23bd0008;
+			    // jr $ra
+			    8'd10:   Instruction <= 32'h03e00008;
+			    // L1:
+			    // addi $a0, $a0, -1
+			    8'd11:   Instruction <= 32'h2084ffff;
+			    // jal sum
+			    8'd12:   Instruction <= 32'h0c100003;
+			    // lw $a0, 0($sp)
+			    8'd13:   Instruction <= 32'h8fa40000;
+			    // lw $ra, 4($sp)
+			    8'd14:   Instruction <= 32'h8fbf0004;
+			    // addi $sp, $sp, 8
+			    8'd15:   Instruction <= 32'h23bd0008;
+			    // add $v0, $a0, $v0
+			    8'd16:   Instruction <= 32'h00821020;
+			    // jr $ra
+			    8'd17:   Instruction <= 32'h03e00008;
+			
+			    default: Instruction <= 32'h00000000;
+		    endcase
+		
+    endmodule
+    
+    ```
+    
     
     
     
